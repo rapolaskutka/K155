d25b7ed5b56c2847245e8bfe589dda22aacc9239
maneris <mantkveder@gmail.com>
2022-04-06 21:53:32 +0300

    kvietimas padarytas tiktai nesusisiekia su conteineriu

--- client/src/pages/Exhibit.js
+++ client/src/pages/Exhibit.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Exhibit = () => {
-  const params = useParams();
-  return <div>exhibit with id: {params.exhibitId}</div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const params = useParams();
+    fetch('/api/v1/page/' + params.exhibitId)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`This is an HTTP error: The status is ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  console.log(data);
+  console.log(error);
+  console.log(loading);
+  return (
+    <div>
+      {loading && <div>A moment please...</div>}
+      {error && <div>{`There is a problem fetching the post data - ${error}`}</div>}
+      <p>{data.image_name}</p>
+      <p>{data.disc}</p>
+    </div>
+  );
 };
 
 export default Exhibit;
